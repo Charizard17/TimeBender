8c6739c8b32374ec193968985d48644128301fd7
Charizard17 <3723esad@gmail.com>
2023-07-09 21:37:34 +0200

    Initial Commit

---
+++ TimeStretch/TimeStretchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStretchApp.swift
+//  TimeStretch
+//
+//  Created by Esad Dursun on 09.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TimeStretchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
