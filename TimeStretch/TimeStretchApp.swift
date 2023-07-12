588e2db97430d073fce7a9603e52327b7b2b169a
Charizard17 <3723esad@gmail.com>
2023-07-12 22:38:29 +0200

    notification authorisation request handling

--- TimeStretch/TimeStretchApp.swift
+++ TimeStretch/TimeStretchApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct TimeStretchApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        requestNotificationAuthorization()
+        return true
+    }
+    
+    func requestNotificationAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if let error = error {
+                print("Notification authorization error: \(error.localizedDescription)")
+            } else {
+                print("Notification authorization granted")
+            }
+        }
+    }
+    
 }
