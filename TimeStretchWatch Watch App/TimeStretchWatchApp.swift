3cbd754463f0ed5a6f1d8badcbb5017091ac6692
Charizard17 <3723esad@gmail.com>
2023-07-19 19:22:37 +0200

    watchOS app project created - template

---
+++ TimeStretchWatch Watch App/TimeStretchWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStretchWatchApp.swift
+//  TimeStretchWatch Watch App
+//
+//  Created by Esad Dursun on 19.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TimeStretchWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
